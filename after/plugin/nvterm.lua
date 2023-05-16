6bf048c5326b43971f6388b01482c0a30f92bab9
Serban <serban.ivanescu@gmail.com>
2023-05-16 18:42:03 -0400

    update

--- after/plugin/nvterm.lua
+++ after/plugin/nvterm.lua
@@ -5,6 +5,11 @@ local term = require('nvterm.terminal')
 vim.keymap.set({ 'n', 't' }, '<A-h>', function() term.toggle('horizontal') end)
 vim.keymap.set({ 'n', 't' }, '<A-v>', function() term.toggle('vertical') end)
 vim.keymap.set({ 'n', 't' }, '<A-f>', function() term.toggle('float') end)
+vim.keymap.set({ 't' }, '<C-w>h', function()  vim.cmd("wincmd h") end)
+vim.keymap.set({ 't' }, '<C-w>j', function()  vim.cmd("wincmd j") end)
+vim.keymap.set({ 't' }, '<C-w>k', function()  vim.cmd("wincmd k") end)
+vim.keymap.set({ 't' }, '<C-w>l', function()  vim.cmd("wincmd l") end)
+
 
 
 nvterm.setup()
