8f3b240e3d8456b8452d173d967bdf78aa8de7ec
Serban <serban.ivanescu@gmail.com>
2025-03-30 12:15:26 -0400

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
-require("serban")
+-- vim.cmd("source init.vim")
 
+vim.cmd("set runtimepath^=~/.vim runtimepath+=~/.vim/after")
+vim.cmd("let &packpath=&runtimepath")
+vim.cmd("source ~/.vim/vimrc")
+
+require("serban")
 
